a33886a13b6534668176e0390ab34d37f9f2bea5
Rabin Sedhai <bisheshsedhai12@gmail.com>
2025-08-05 20:42:45 +0545

    modified  metadata

--- app/layout.js
+++ app/layout.js
@@ -4,24 +4,48 @@ import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+});
 
 export const metadata = {
   title: "Mobile Shop",
-  description: "MobileShop with Next.js ",
+  description: "A modern e-commerce platform built with Next.js, MongoDB, Stripe, and Clerk.",
+  metadataBase: new URL("https://mobile-shop-lime.vercel.app"),
+  openGraph: {
+    title: "Mobile Shop",
+    description: "A modern e-commerce platform built with Next.js, MongoDB, Stripe, and Clerk.",
+    url: "https://mobile-shop-lime.vercel.app",
+    siteName: "Mobile Shop",
+    images: [
+      {
+        url: "/thumbnail.jpg", 
+        width: 1200,
+        height: 630,
+        alt: "Mobile Shop Homepage Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mobile Shop",
+    description: "A modern mobile e-commerce platform.",
+    images: ["/thumbnail.jpg"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${outfit.className} antialiased text-gray-700`} >
+        <body className={`${outfit.className} antialiased text-gray-700`}>
           <Toaster />
-          <AppContextProvider>
-            {children}
-          </AppContextProvider>
+          <AppContextProvider>{children}</AppContextProvider>
         </body>
       </html>
-      </ClerkProvider>
+    </ClerkProvider>
   );
 }
